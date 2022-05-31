6d1df986b819618b7b4e020d851746eafb401ee2
Andres Soto <andresegidiosoto@gmail.com>
2022-05-31 14:55:40 -0500

    Update server.R

--- server.R
+++ server.R
@@ -13,15 +13,16 @@ fredr_set_key('dad071a33ef9414bb0a759835d9ec507')
 
 shinyServer(function(input, output) {
   
-  #### Overview
   
-  output$distPlot <- renderPlot({
+  ## First Tab: S&P - Valuation
+  
+  output$distPlot_sp_val <- renderPlot({
     
-    if (input$metric == 'DGS10'){metricName <- '10Yr Yield'}
+    if (input$metric_SP_valuation == "SP500"){metricName <- "S&P500 - Valuation"}
     
-    timeSeries <- fredr(input$metric, 
-                        observation_start = input$start,
-                        observation_end = input$end) %>%
+    timeSeries <- fredr(input$metric_SP_valuation, 
+                        observation_start = input$start_sp_val,
+                        observation_end = input$end_sp_val) %>%
       drop_na() %>%
       select(1,3) %>%
       rename(Close = 2)
@@ -36,20 +37,19 @@ shinyServer(function(input, output) {
     
   })
   
-  ## Rates
   
-  output$distPlot2 <- renderPlot({
+  ## Second Tab: S&P - Growth
+  
+  output$distPlot_SP_growth <- renderPlot({
     
+    metricName <- ifelse(input$metric_SP_growth == "A", "Sales PS ", 
+                         ifelse(input$metric_SP_growth == "B", "Sales Growth",
+                                ifelse(input$metric_SP_growth == "C", "EPS- NTM",
+                                       ifelse(input$metric_SP_growth == "D", "EPS",NULL))))
     
-    
-    metricName <- ifelse(input$metric2 == "DTB3", "3mo Tbill", 
-                         ifelse(input$metric2 == 'T10Y2Y', "2-10 Spread",
-                                ifelse(input$metric2 == 'MORTGAGE30US', "30Y Mortgage", NULL)))
-    
-    
-    timeSeries <- fredr(input$metric2, 
-                        observation_start = input$start2,
-                        observation_end = input$end2) %>%
+    timeSeries <- fredr(input$metric_SP_growth, 
+                        observation_start = input$start_sp_growth,
+                        observation_end = input$end_sp_growth) %>%
       drop_na() %>%
       select(1,3) %>%
       rename(Close = 2)
@@ -64,21 +64,54 @@ shinyServer(function(input, output) {
     
   })
   
-  ## Inflation
+
+ 
+  ## Third Tab: Rates
   
-  output$distPlot3 <- renderPlot({
+  output$distPlot_rates <- renderPlot({
     
     
     
-    metricName <- ifelse(input$metric3 == 'DFII10', "10Y TIPS", 
-                         ifelse(input$metric3 == 'T10YIE', "10Y Breakeven Inflation",
-                                ifelse(input$metric3 == 'T5YIE', "5Y Breakeven Inflation",
-                                       ifelse(input$metric3 == 'T5YIFR', "5,5 Forward Inflation", NULL))))
+    metricName <- ifelse(input$metric_rates == "DTB3", "3mo Tbill", 
+                         ifelse(input$metric_rates == 'T10Y2Y', "2-10 Spread",
+                                ifelse(input$metric_rates == 'MORTGAGE30US', "30Y Mortgage",
+                                       ifelse(input$metric_rates == 'DGS10', "10Yr Yield", NULL))))
     
     
-    timeSeries <- fredr(input$metric3, 
-                        observation_start = input$start3,
-                        observation_end = input$end3) %>%
+    timeSeries <- fredr(input$metric_rates, 
+                        observation_start = input$start_rates,
+                        observation_end = input$end_rates) %>%
+      drop_na() %>%
+      select(1,3) %>%
+      rename(Close = 2)
+    
+    timeSeries <- xts(timeSeries[,2], order.by = timeSeries$date)
+    
+    chartSeries(timeSeries,
+                name = metricName,
+                type="line",
+                theme=chartTheme('white'))
+    
+    
+  })
+  
+  ## Fourth Tab: Inflation
+  
+  output$distPlot_inflation <- renderPlot({
+    
+    
+    
+    metricName <- ifelse(input$metric_inflation == 'DFII10', "10Y TIPS", 
+                         ifelse(input$metric_inflation == 'T10YIE', "10Y Breakeven Inflation",
+                                ifelse(input$metric_inflation == 'T5YIE', "5Y Breakeven Inflation",
+                                       ifelse(input$metric_inflation == 'T5YIFR', "5,5 Forward Inflation",
+                                              ifelse(input$metric_inflation  == "CPIAUCSL", "CPI",
+                                                     ifelse(input$metric_inflation == "PPIACO",  "PPI",NULL))))))
+    
+    
+    timeSeries <- fredr(input$metric_inflation, 
+                        observation_start = input$start_inflation,
+                        observation_end = input$end_inflation) %>%
       drop_na() %>%
       select(1,3) %>%
       rename(Close = 2)
